8e0877c44647a63b855f54ff1e716db1ba7f3bde
xavijam <xavijam@gmail.com>
2013-01-29 00:20:30 +0100

    Icons and width fixed!

--- Clocky/AppDelegate.m
+++ Clocky/AppDelegate.m
@@ -14,26 +14,33 @@
 @synthesize statusBar   = _statusBar;
 
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
-}
+    - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+    {
+        // Insert code here to initialize your application
+    }
 
 
-- (void) awakeFromNib {
-    self.statusBar = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-    
-    self.statusBar.title = [NSImage imageNamed:@"clock.png"];
-    
-    // you can also set an image
-    self.statusBar.image = [NSImage imageNamed:@"clock.png"];
-    //NSImage* icon = [NSImage alloc] initWith ...]
-    //[statusItem setImage:icon];
-    //-(void)setImage:(NSImage *)icon.png
-    
-    self.statusBar.menu = self.statusMenu;
-    self.statusBar.highlightMode = YES;
-}
+    - (void) awakeFromNib {
+        self.statusBar = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+        
+        // Set menu with status bar
+        NSFont* font1= [NSFont fontWithName:@"Lucida Sans" size:14.0];
+        self.statusMenu.minimumWidth = 275;
+        self.statusMenu.font = font1;
+        self.statusBar.menu = self.statusMenu;
+
+        //Sets the images in the NSStatusItem
+        NSImage* image = [NSImage imageNamed:@"clock.png"];
+        NSImage* image_highlighted = [NSImage imageNamed:@"clock_highlighted.png"];
+        self.statusBar.image = image;
+        self.statusBar.alternateImage = image_highlighted;
+        
+        //Sets the mouse over text
+        self.statusBar.toolTip = @"Edit your locations";
+        
+        //Enables highlighting
+        self.statusBar.highlightMode = YES;
+    }
 
 
 
